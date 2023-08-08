2d87962db342f5932a77ad6991f6124be87239cb
IFM-Facility-management <141722510+IFM-Facility-management@users.noreply.github.com>
2023-08-08 12:30:26 +0300

    Add files via upload

---
+++ Assets/javascript/main.js
@@ -0,0 +1,21 @@
+/** @format */
+"use strict";
+
+const stone = "eqlibrium";
+
+let swiperTestimonial = new Swiper(".testimonial__container", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 48,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    568: {
+      slidesPerView: 2,
+    },
+  },
+});
